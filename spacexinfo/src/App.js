e7397e3fed1d35047155bd8e614fe856540bb538
Adam Lloyd <lloyd.adam99@gmail.com>
2022-05-25 12:43:14 +0200

    Skapat en navbar
    Renderar en "frame" beroende på val
    
    Co-Authored-By: kinloklee <81237262+kinloklee@users.noreply.github.com>
    Co-Authored-By: Pontus Nordström <61506326+pontusnordstrom89@users.noreply.github.com>

--- spacexinfo/src/App.js
+++ spacexinfo/src/App.js
@@ -1,25 +1,58 @@
+import { useState } from 'react';
 import './App.css';
 import Launches from './Components/Launches/Launches';
+import Mainframe from './Components/Mainframe';
+import Navbar from './Components/Navbar';
 import Rockets from './Components/Rockets/Rockets';
 
 function App() {
+  const [mainFrame, setMainFrame] = useState([
+    {
+      id: 1,
+      title: "Home",
+      frame: <Mainframe />
+      },
+    {
+      id: 2,
+      title: "Launches"
+      ,
+      frame: <Launches />
+    },
+    {
+      id: 3,
+      title: "Rockets"
+      ,
+      frame: <Rockets />
+    },
+    {
+      id: 4,
+      title: "My launches"
+      ,
+      frame: <Mainframe />
+    }
+]);
+
+  const [frame, setFrame] = useState(<Mainframe />)
+
+function changeFrame(id) {
+  setFrame(id)
+}
+
   return (
-    <main className="container row m-auto">
+    <div>
+      <ul className="list-group">
+        <Navbar items={mainFrame} changeFrame={changeFrame} />
+      </ul>
 
-      <h1 className='text-center'>SpaceX info</h1>
-      
-      <div className='col-lg-6'>
-        <h2>Launches</h2>
-        <Launches />
-      </div>
+      <main className="container row m-auto">
 
-      <div className='col-lg-6'>
-        <h2>Rockets</h2>
-        <Rockets />
-      </div>
-      
-  
-    </main>
+        <h1 className='text-center'>SpaceX info</h1>
+        
+        <div className='col-lg-12'>
+          {frame}
+        </div>
+      </main>
+    </div>
   );
 }
 
